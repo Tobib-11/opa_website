d8cc4147bb132d7516343c6995b249d5bd79dca7
Tobib-11 <tobias.beran@student.htldornbirn.at>
2026-05-20 15:15:10 +0200

    20.05.2026 aufklapp menü index angefangen und andere seiten erstellt

---
+++ javascript/scriptForAll.js
@@ -0,0 +1,5 @@
+//aufklapp dings top
+function TopToggleMenu() {
+      const menu = document.getElementById('menuTop');
+      menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    }
