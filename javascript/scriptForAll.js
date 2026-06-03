b761f80ac58633da57e88c42c1a8266598eb1323
Tobib-11 <tobias.beran@student.htldornbirn.at>
2026-06-03 16:33:15 +0200

    03.06.2026

--- javascript/scriptForAll.js
+++ javascript/scriptForAll.js
@@ -1,5 +1,23 @@
-//aufklapp dings top
+// TOP überallaufklapp dings top
 function TopToggleMenu() {
-      const menu = document.getElementById('menuTop');
-      menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-    }
+  const menu = document.getElementById("menuTop");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+//INDEX picturechange für sscnhaps
+
+function IndexPictureChangeSchnaps() {
+  let pictures = [
+    "/pictures/INDEXschnapschange/schnaps1",
+    "pictures/INDEXschnapschange/schnaps2",
+  ];
+
+  let buttons = document.querySelectorAll('.indexSchnapsVorstellung button');
+  let firstPic = document.getElementByclass('FirstSchnapsPic');
+
+  function updatePictures(){
+
+  }
+
+
+}
